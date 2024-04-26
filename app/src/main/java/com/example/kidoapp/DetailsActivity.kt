4887b1ed6c1b27e7e36e867a82c991467914820a
Ahmed Dan <100492004+AhmedEzzi@users.noreply.github.com>
2024-04-26 04:19:35 +0300

    updated
    updated

--- app/src/main/java/com/example/kidoapp/DetailsActivity.kt
+++ app/src/main/java/com/example/kidoapp/DetailsActivity.kt
@@ -10,6 +10,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
+
 class DetailsActivity : AppCompatActivity() {
     lateinit var wmTxt:TextView
     override fun onCreate(savedInstanceState: Bundle?) {
