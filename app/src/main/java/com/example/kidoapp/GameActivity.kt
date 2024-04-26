719769ac12297b88afd9a8b15a19e40c2c107b27
Ahmed Dan <100492004+AhmedEzzi@users.noreply.github.com>
2024-04-26 12:11:38 +0300

    voice update
    voice been added when u press

--- app/src/main/java/com/example/kidoapp/GameActivity.kt
+++ app/src/main/java/com/example/kidoapp/GameActivity.kt
@@ -2,11 +2,13 @@ package com.example.kidoapp
 
 import android.content.pm.ActivityInfo
 import android.os.Bundle
+import android.speech.tts.TextToSpeech
 import android.view.View
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+import android.window.OnBackInvokedDispatcher
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,12 +17,13 @@ import com.daimajia.androidanimations.library.Techniques
 import com.daimajia.androidanimations.library.YoYo
 import com.google.android.material.color.utilities.Score
 
-class GameActivity : AppCompatActivity() {
+class GameActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
     lateinit var  imageGame:ImageView
     lateinit var answerTxt:EditText
     lateinit var random:imageList
     lateinit var test:TextView
     lateinit var scoreTxt:TextView
+    lateinit var tts: TextToSpeech
     var score=0
 
     val animalList = listOf(
@@ -44,7 +47,7 @@ class GameActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_game)
         requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
-
+        tts = TextToSpeech(this, this)
         imageGame=findViewById(R.id.imageGame)
         answerTxt=findViewById(R.id.answerTxt)
         scoreTxt=findViewById(R.id.scoreTxt)
@@ -77,5 +80,25 @@ class GameActivity : AppCompatActivity() {
                 Toast.makeText(this, "WRONG", Toast.LENGTH_SHORT).show()
 
         }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        tts.stop()
+    }
+
+    override fun getOnBackInvokedDispatcher(): OnBackInvokedDispatcher {
+        return super.getOnBackInvokedDispatcher()
+        tts.stop()
+    }
+    override fun onInit(status: Int) {
+                tts.setSpeechRate(0.7.toFloat())
+        tts.setPitch(0.7.toFloat())
+    }
+
+    fun soundBtn(view: View) {
+    val speak = random.name
+        tts.speak(speak,TextToSpeech.QUEUE_FLUSH,null,null)
+
     }
 }
